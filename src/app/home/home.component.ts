bd296c45493ed575d63debac4037559790dcadef
stuartwilliamchaulk <stuart.chaulk@gmail.com>
2017-03-14 17:54:40 -0400

    tweaks

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,13 +14,9 @@ import 'rxjs/add/operator/do';
   styleUrls: [ './home.component.css' ],
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements AfterViewInit {
   @ViewChild('button1') public button1: ElementRef;
   private  button1Obs: Observable<any>;
-
-  public ngOnInit() {
-    console.log('hello `Home` component');
-  }
 
   public ngAfterViewInit() {
     this.button1Obs = Observable.fromEvent(this.button1.nativeElement, 'click');
